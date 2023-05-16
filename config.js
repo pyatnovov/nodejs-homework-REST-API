152b99758da99bfac2cee24a1bb4208ac2aa55f8
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-05-16 09:57:29 +0300

    Add branch and database

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST =
+  "mongodb+srv://pyatnovov:QZuMGDMIQP0JOKdE@cluster0.lml0wzh.mongodb.net/db-contacts?retryWrites=true&w=majority";
+module.exports = {
+  DB_HOST,
+};
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+const { DB_HOST } = require("./config");
 
-app.listen(4000, () => {
-  console.log("Server running. Use our API on port: 4000");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(4000, () => {
+      console.log("Server running. Use our API on port: 4000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
